909978959f27c67f27ffdaf9b958cd1713506cd5
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-03-25 22:36:47 +0200

    Updates

---
+++ precache-manifest.1a3544d4c4b4f4df7ec9c28db22f4740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ef0efba1b22d81bf33a61a5c07d7e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60a139b9327c03bea6d9",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "4d0e06b2ee380f05bd08",
+    "url": "./static/css/main.9f604ba9.chunk.css"
+  },
+  {
+    "revision": "60a139b9327c03bea6d9",
+    "url": "./static/js/2.d10b847f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d10b847f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0e06b2ee380f05bd08",
+    "url": "./static/js/main.d1883ef2.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.1a3544d4c4b4f4df7ec9c28db22f4740.js"
 );
 
 self.addEventListener('message', (event) => {
